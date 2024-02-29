c89dab0c3fdcf479b09868591d5c2de075771f4c
marinarosenwald <marinarosenwald@gmail.com>
2024-02-28 22:17:04 -0800

    updated config

--- LLMs/config.py
+++ LLMs/config.py
@@ -1,29 +1,29 @@
 FALCON_7B = {
     "model_name": "Falcon-7b",
     "hf_model_path": "tiiuae/falcon-7b",
-    "save_path": "/deleteLater/Falcon",
+    "save_path": "/Users/marirosenwald/CSS583_KMS_LLMS/deleteLater/Falcon",
 }
 
 VICUNA_7b = {
     "model_name": "Vicuna-7b",
     "hf_model_path": "lmsys/vicuna-7b-v1.5",
-    "save_path": "/deleteLater/Vicuna",
+    "save_path": "/Users/marirosenwald/CSS583_KMS_LLMS/deleteLater/Vicuna",
 }
 
 BART_LARGE = {
     "model_name": "Bart-Large",
     "hf_model_path": "facebook/bart-large-cnn",
-    "save_path": "/deleteLater/Bart",
+    "save_path": "/Users/marirosenwald/CSS583_KMS_LLMS/deleteLater/Bart",
 }
 
 FALCONSAI = {
     "model_name": "FalconSai",
     "hf_model_path": "Falconsai/text_summarization",
-    "save_path": "/deleteLater/FalconSai",
+    "save_path": "/Users/marirosenwald/CSS583_KMS_LLMS/deleteLater/FalconSai",
 }
 
 LED_LARGE_BOOK = {
     "model_name": "LED-Large-Book-Sum",
     "hf_model_path": "pszemraj/led-large-book-summary",
-    "save_path": "/deleteLater/LED",
+    "save_path": "/Users/marirosenwald/CSS583_KMS_LLMS/deleteLater/LED",
 }
